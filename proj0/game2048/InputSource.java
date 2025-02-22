76b7b365e0d1338da79b60e61862998d91e3f630
shy_vector <shy_vector@m.scnu.edu.cn>
2025-02-23 00:58:19 +0800

    done with proj0: 2048 Game

--- proj0/game2048/InputSource.java
+++ proj0/game2048/InputSource.java
@@ -1,16 +1,19 @@
 package game2048;
 
-/** Describes a source of input commands.
- *  @author P. N. Hilfinger
+/**
+ * Describes a source of input commands.
+ * 
+ * @author P. N. Hilfinger
  */
 interface InputSource {
 
-    /** Returns one command string. */
-    String getKey();
+  /** Returns one command string. */
+  String getKey();
 
-    /** Returns a candidate Tile whose row and column is in the range
-     *  0 .. SIZE-1.  */
-    Tile getNewTile(int size);
+  /**
+   * Returns a candidate Tile whose row and column is in the range
+   * 0 .. SIZE-1.
+   */
+  Tile getNewTile(int size);
 
 }
-
